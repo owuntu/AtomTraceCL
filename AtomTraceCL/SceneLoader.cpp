396227dcd60947c760ad6de77f01a0bd86edca84
Yuntao <cktao.g@gmail.com>
2018-01-08 18:14:59 +0800

    Separate Cornel box loading.

--- AtomTraceCL/SceneLoader.cpp
+++ AtomTraceCL/SceneLoader.cpp
@@ -10,46 +10,12 @@
 
 namespace AtomTraceCL
 {
-    bool LoadUnitTestObj(ObjectList& oList)
+    void LoadCornelBox(ObjectList& oList)
     {
         Plane pl0;
-        Sphere s0;
         DiffuseMaterial wall;
         wall.SetColor(AtomMathCL::Vector3(1.0f, 1.0f, 1.0f));
 
-        DiffuseMaterial ball;
-        ball.SetColor(AtomMathCL::Vector3(1.f, 0.3f, 0.3f));
-
-        Metal metal;
-        // Hard coded cooper data.
-        metal.m_eta = (AtomMathCL::Vector3(0.2f, 0.924f, 1.102f));
-        metal.m_kk = AtomMathCL::Vector3(3.91f, 2.45f, 2.14f);
-
-        // light
-        {
-            RenderObject obj;
-            obj.SetGeometry(&s0);
-            obj.Scale(1.0f);
-            obj.Translate(1.0f, -22.f, 10.f);
-
-            Light l0;
-            l0.SetEmission(AtomMathCL::Vector3(1.f));
-            obj.SetMaterial(&l0);
-
-            oList.AddObject(obj);
-        }
-
-        {
-            TriMesh tMesh;
-            tMesh.LoadObjFromFile("scene\\plane.obj");
-            RenderObject obj;
-            obj.SetGeometry(&tMesh);
-            //obj.Rotate(90.f, AtomMathCL::Vector3::UNIT_X);
-            //obj.Translate(0.f, 2.f, 0.f);
-            obj.SetMaterial(&ball);
-            oList.AddObject(obj);
-        }
-#if 1
         // back
         {
             RenderObject obj;
@@ -123,12 +89,50 @@ namespace AtomTraceCL
             obj.SetMaterial(&wall);
             oList.AddObject(obj);
         }
-#endif
+    }
+
+    bool LoadUnitTestObj(ObjectList& oList)
+    {
+        Sphere s0;
+
+        DiffuseMaterial ball;
+        ball.SetColor(AtomMathCL::Vector3(1.f, 0.3f, 0.3f));
+
+        Metal metal;
+        // Hard coded cooper data.
+        metal.m_eta = (AtomMathCL::Vector3(0.2f, 0.924f, 1.102f));
+        metal.m_kk = AtomMathCL::Vector3(3.91f, 2.45f, 2.14f);
+
+        // light
+        {
+            RenderObject obj;
+            obj.SetGeometry(&s0);
+            obj.Scale(1.0f);
+            obj.Translate(1.0f, -22.f, 10.f);
+
+            Light l0;
+            l0.SetEmission(AtomMathCL::Vector3(1.f));
+            obj.SetMaterial(&l0);
+
+            oList.AddObject(obj);
+        }
+
+        {
+            TriMesh tMesh;
+            tMesh.LoadObjFromFile("scene\\teapot-low.obj");
+            RenderObject obj;
+            obj.SetGeometry(&tMesh);
+            //obj.Rotate(90.f, AtomMathCL::Vector3::UNIT_X);
+            //obj.Translate(0.f, 2.f, 0.f);
+            obj.SetMaterial(&ball);
+            oList.AddObject(obj);
+        }
+        LoadCornelBox(oList);
         return true;
     }
     bool LoadScene(ObjectList& oList)
     {
-        return LoadUnitTestObj(oList);
+        //return LoadUnitTestObj(oList);
 
         Plane pl0;
         Sphere s0;
@@ -176,81 +180,7 @@ namespace AtomTraceCL
             oList.AddObject(obj);
         }
 
-        // back
-        {
-            RenderObject obj;
-            obj.SetGeometry(&pl0);
-            obj.Scale(50.f);
-            obj.Rotate(90.f, AtomMathCL::Vector3::UNIT_X);
-            obj.Translate(0.f, 0.f, -30.f);
-
-            obj.SetMaterial(&wall);
-            oList.AddObject(obj);
-        }
-
-        // front
-        {
-            RenderObject obj;
-            obj.SetGeometry(&pl0);
-            obj.Scale(50.f);
-            obj.Rotate(-90.f, AtomMathCL::Vector3::UNIT_X);
-            obj.Translate(0.f, 0.f, 70.f);
-
-            obj.SetMaterial(&wall);
-            oList.AddObject(obj);
-        }
-#if 1
-        // left
-        {
-            RenderObject obj;
-            obj.SetGeometry(&pl0);
-            obj.Scale(50.f);
-            obj.Rotate(-90.f, AtomMathCL::Vector3::UNIT_Z);
-            obj.Translate(-15.f, 0.f, 0.f);
-
-            DiffuseMaterial m0;
-            m0.SetColor(AtomMathCL::Vector3(0.25f, 0.25f, 0.75f));
-            obj.SetMaterial(&m0);
-            oList.AddObject(obj);
-        }
-
-        // right
-        {
-            RenderObject obj;
-            obj.SetGeometry(&pl0);
-            obj.Scale(50.f);
-            obj.Rotate(90.f, AtomMathCL::Vector3::UNIT_Z);
-            obj.Translate(15.f, .0f, .0f);
-
-            DiffuseMaterial m0;
-            m0.SetColor(AtomMathCL::Vector3(0.25f, 0.75f, 0.25f));
-            obj.SetMaterial(&m0);
-            oList.AddObject(obj);
-        }
-
-        // top
-        {
-            RenderObject obj;
-            obj.SetGeometry(&pl0);
-            obj.Scale(50.f);
-            obj.Rotate(180.f, AtomMathCL::Vector3::UNIT_X);
-            obj.Translate(.0f, 0.f, .0f);
-
-            obj.SetMaterial(&wall);
-            oList.AddObject(obj);
-        }
-
-        // bottom
-        {
-            RenderObject obj;
-            obj.SetGeometry(&pl0);
-            obj.Scale(50.f);
-            obj.Translate(0.f, -22.f, 0.f);
-
-            obj.SetMaterial(&wall);
-            oList.AddObject(obj);
-        }
-#endif
+        LoadCornelBox(oList);
         return true;
     }
 } // namespace AtomTraceCL
